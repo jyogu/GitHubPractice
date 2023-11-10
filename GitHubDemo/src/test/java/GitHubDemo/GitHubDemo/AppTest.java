0bf0a0fb61238c8f6f0271b457b7d3829024a5d8
jyogu <jyogu@host.docker.internal>
2023-11-10 14:18:00 +0530

    commit on 10th nov

---
+++ GitHubDemo/src/test/java/GitHubDemo/GitHubDemo/AppTest.java
@@ -0,0 +1,20 @@
+package GitHubDemo.GitHubDemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
